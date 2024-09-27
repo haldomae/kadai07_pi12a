53f9f80baad4152a88b485d4a52682077bda8c9c
haldomae <hal.domae@gmail.com>
2024-09-27 16:38:55 +0900

    ListViewにテキストを表示

--- app/src/main/java/com/hal_domae/kadai07_pi12/MainActivity.kt
+++ app/src/main/java/com/hal_domae/kadai07_pi12/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.hal_domae.kadai07_pi12
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -19,5 +20,25 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val data = listOf(
+            "モンスター01",
+            "モンスター02",
+            "モンスター03",
+            "モンスター04",
+            "モンスター05",
+            "モンスター06",
+            "モンスター07",
+            "モンスター08",
+        )
+
+        // 1つ目の引数thisはコンテキスト(アプリやアクティビティの情報)
+        // 2つ目の引数はリストの各項目に使うレイアウト(R.layout.simple_expandable_list_item_1はテキスト項目が1つだけあるレイアウト)
+        // 3つ目の引数はリストに表示するデータ
+        binding.list.adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_expandable_list_item_1,
+            data
+        )
     }
 }
